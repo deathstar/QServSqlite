17a5e856ec97b26a07fddd2de8bcbcc0b630ed4c
George Scott <x90265@gmail.com>
2026-05-31 14:28:22 -0700

    loop termination

--- mod/QServ.cpp
+++ mod/QServ.cpp
@@ -397,7 +397,7 @@ namespace server {
         int i=0;
         int j=0;
         
-        for(int i; i < strlen(w1); i++)
+        for(int i = 0; i < strlen(w1); i++)
         {
             if(w1[i] == w2[j])
             {
