a0c3649742ead7c49055adae41f5a77f5ecfdf80
George Scott <x90265@gmail.com>
2026-05-31 13:36:01 -0700

    accidental overwrite

--- mod/QCom.h
+++ mod/QCom.h
@@ -1,156 +1,70 @@
-#ifndef QSERV_H_INCLUDED
-#define QSERV_H_INCLUDED
+#ifndef QCOM_H_INCLUDED
+#define QCOM_H_INCLUDED
 
-#include "game.h"
-#include <cstdint>
-#include "fpsgame.h"
-#include "../GeoIP/libGeoIP/GeoIP.h"
-#include <time.h>
-#include <pthread.h>
-#include "../ircbot/ircbot.h"
-#include <iostream>
-#include <sstream>
+#include "QServ.h"
 
-
-const bool olanguagewarn = false;
-const int maxolangwarnings = 5;
-const char commandprefix = '#';
-
-static char const *qserv_modenames[] = {
-"ffa", "coop", "teamplay", "insta", "instateam", "effic",
-    "efficteam", "tac", "tacteam", "capture", "regencapture",
-    "ctf", "instactf", "protect", "instaprotect", "hold", "instahold",
-    "efficctf", "efficprotect", "effichold", "collect", "instacollect", "efficcollect"
-};
-
-static string owords[] = {
-	"fuck", "shit", "cunt", "bitch", "whore", "twat",
-    "faggot", "retard", "pussy", "ass",
-    "vagina", "slut", "nigger", "queer", "gaylord", "gay",
-    "homosexual", "semen", "creampie", "dick",
-    "handjob", "blowjob", "cock", "tits", "penis"
-};
-
-static char bunny[] = "\f1('\\_/')\n(\f5=\f1'.'\f5=\f1)\n('')_('')";
-
-struct SCommand {
-    char name[50];
-    char desc[500];
-    int priv;
-    int id;
-    void (*func)(int, char**, int);
-    bool hasargs;
-    int args;
-};
+#define ncommand(name, desc, priv, callback, args) { qs.newcommand(name, desc, priv, callback, args); }
+#define QSERV_CALLBACK void
+#define p int cid, char **args, int argc
+#define CMD_NAME(id) qs.getCommandName(id)
+#define CMD_DESC(id) qs.getCommandDesc(id)
+#define CMD_PRIV(id) qs.getCommandPriv(id)
+#define CMD_LAST     qs.getlastCommand()
+#define CMD_SENDER   qs.getSender()
+#define CMD_SCI      qs.getlastCI()
+#define CMD_SA       qs.getlastSA()
+#define fulltext     qs.getFullText()
 
 namespace server {
-    struct msg {
-        int count;
-        double time;
-    };
-
-    class QServ {
-        public:
-            QServ(bool, int, char);
-            ~QServ();
-            
-            int instacoop_gamelimit;
-
-            bool initgeoip(const char*);
-            bool initcitygeoip(const char*);
-            char *congeoip(const char*);
-            //char *citygeoip(const char*);
-            //char *regiongeoip(const char*);
-        
-            std::string cgip(const char*);
-
-            void newcommand(const char*, const char*, int, void (*)(int, char**, int), int);
-            bool isCommand(char*);
-            int getCommand(char*, char**);
-            void exeCommand(int, char**, int);
-
-            char findWord(char*, char*, bool);
-
-            char *getCommandName(int);
-            char *getCommandDesc(int);
-            int getCommandPriv(int);
-            bool commandHasArgs(int);
-            int getCommandArgCount(int);
-            int getlastCommand();
-
-            void checkoLang(int, char*);
-            void setoLangWarn(int);
-            void resetoLangWarn(int);
-            int getoLangWarn(int);
-
-            void initCommands(void (*)());
-
-            void setFullText(const char*);
-            char *getFullText();
-
-            void setSender(int);
-            int getSender();
-
-            void setlastCI(clientinfo*);
-            clientinfo getlastCI();
-
-            void setlastSA(bool);
-            bool getlastSA();
-            char *cntoip(int);
-
-            clientinfo *getClient(int);
-
-            static clientinfo m_lastCI;
-
-            bool handleTextCommands(clientinfo*, char*);
-            bool enable_HTTP_geo;
-
-            bool isLangWarnOn();
-            void setoLang(bool);
-
-            void setCmdPrefix(unsigned char);
-            char getCmdPrefix();
-
-            void getLocation(clientinfo*);
-
-            void checkMsg(int);
-            int getMsgC(int);
-            void resetMsg(int);
-        protected:
-            GeoIP *m_geoip;
-            GeoIP *city_geoip;
-            int m_lastcommand;
-            SCommand m_command[50];
-            int m_oLangWarn[1000];
-            int m_oTimes;
-            void (*m_checkolangCallback)(int, char*);
-            char m_fulltext[1024];
-            int m_sender;
-            bool m_lastSA;
-            int m_owordcount;
-            bool m_olangcheck;
-            int m_maxolangwarns;
-            char m_cmdprefix;
-
-            msg ms[1000];
-    };
+    extern void initCmds();
+    extern QSERV_CALLBACK me_cmd(p);
+    extern QSERV_CALLBACK stats_cmd(p);
+    extern QSERV_CALLBACK localtime_cmd(p);
+    extern QSERV_CALLBACK time_cmd(p);
+    extern QSERV_CALLBACK bunny_cmd(p);
+    extern QSERV_CALLBACK owords_cmd(p);
+    extern QSERV_CALLBACK olangfilter_cmd(p);
+    extern QSERV_CALLBACK echo_cmd(p);
+    extern QSERV_CALLBACK revokepriv_cmd(p);
+    extern QSERV_CALLBACK forceintermission_cmd(p);
+    extern QSERV_CALLBACK getversion_cmd(p);
+    extern QSERV_CALLBACK callops_cmd(p);
+    extern QSERV_CALLBACK pm_cmd(p);
+    extern QSERV_CALLBACK sendprivs_cmd(p);
+    extern QSERV_CALLBACK forgive_cmd(p);
+    extern QSERV_CALLBACK forcespectator_cmd(p);
+    extern QSERV_CALLBACK unspectate_cmd(p);
+    extern QSERV_CALLBACK mute_cmd(p);
+    extern QSERV_CALLBACK unmute_cmd(p);
+    extern QSERV_CALLBACK editmute_cmd(p);
+    extern QSERV_CALLBACK uneditmute_cmd(p);
+    extern QSERV_CALLBACK togglelockspec_cmd(p);
+    extern QSERV_CALLBACK uptime_cmd(p);
+    extern QSERV_CALLBACK whois_cmd(p);
+    extern QSERV_CALLBACK help_cmd(p);
+    extern QSERV_CALLBACK cheater_cmd(p);
+    extern QSERV_CALLBACK mapsucks_cmd(p);
+    extern QSERV_CALLBACK ban_cmd(p);
+    extern QSERV_CALLBACK pban_cmd(p);
+    extern QSERV_CALLBACK clearpbans_cmd(p);
+    extern QSERV_CALLBACK teampersist_cmd(p);
+    extern QSERV_CALLBACK invadmin_cmd(p);
+    extern QSERV_CALLBACK allowmaster_cmd(p);
+    extern QSERV_CALLBACK kill_cmd(p);
+    extern QSERV_CALLBACK rename_cmd(p);
+    extern QSERV_CALLBACK addkey_cmd(p);
+    extern QSERV_CALLBACK reloadconfig_cmd(p);
+    extern QSERV_CALLBACK listbans_cmd(p);
+    extern QSERV_CALLBACK unkickban_cmd(p);
+    extern QSERV_CALLBACK syncauth_cmd(p);
+    extern QSERV_CALLBACK smartbot_cmd(p);
+    extern QSERV_CALLBACK cw_cmd(p);
+    extern QSERV_CALLBACK duel_cmd(p);
+    extern QSERV_CALLBACK coopgamelimit_cmd(p);
+    extern QSERV_CALLBACK listmaps_cmd(p);
+    extern QSERV_CALLBACK savemap_cmd(p);
+    extern QSERV_CALLBACK autosendmap_cmd(p);
+    extern QSERV_CALLBACK loadmap_cmd(p);
 }
-
-enum
-{
-    ECHO_ALL = 0,
-    ECHO_IRC ,
-    ECHO_CONSOLE,
-    ECHO_SERV,
-    ECHO_NOCOLOR,
-};
-extern void out(int type, const char *fmt, ...);
-
-#define toip(cn) qs.cntoip(cn)
-
-extern server::QServ qs;
-
-extern int count;
-extern int msgcount[128];
 
 #endif
