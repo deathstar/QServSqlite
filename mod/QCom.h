e3f41d7fa02fdbb355c857996d3537962caaca53
George Scott <x90265@gmail.com>
2026-06-25 00:00:44 -0700

    add flagrun cmd to header

--- mod/QCom.h
+++ mod/QCom.h
@@ -64,6 +64,7 @@ namespace server {
     extern QSERV_CALLBACK savemap_cmd(p);
     extern QSERV_CALLBACK autosendmap_cmd(p);
     extern QSERV_CALLBACK loadmap_cmd(p);
+    extern QSERV_CALLBACK flagrun_cmd(p);
     //extern QSERV_CALLBACK owords_cmd(p);
 }
 
