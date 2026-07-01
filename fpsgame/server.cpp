7d645122a4a648a1b19849ef203f78c04be7c6f3
George Scott <x90265@gmail.com>
2026-06-30 23:43:20 -0700

    properly verify clients for update_team_counters

--- fpsgame/server.cpp
+++ fpsgame/server.cpp
@@ -1103,9 +1103,9 @@ namespace server {
     	team_good_count = 0;
     	team_evil_count = 0;
         loopv(clients) {
-			if (clients[i] == NULL) continue;
-			if (clients[i]->team == NULL) continue;
-            if (clients[i]->state.state == CS_SPECTATOR) continue;
+			if (!clients[i] || !clients[i]->team[0] || clients[i]->state.state == CS_SPECTATOR) {
+				continue;
+			}
             if (strcmp(clients[i]->team, "good") == 0) team_good_count++;
             else if (strcmp(clients[i]->team, "evil") == 0) team_evil_count++;
         }
